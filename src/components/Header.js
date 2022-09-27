cdcc4405c4c62c7d6cf69a65d34e582b2d7acd7f
Bibek Gaire <gairebibek8@gmail.com>
2022-09-27 12:43:21 -0500

    Components updated

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, {} from "react";
+
+function Header() {
+        /* 
+        Return a <h1> element with a description of your app.
+        */
+
+        return (
+                <h1>Reminder App</h1>
+        )
+
+}
+
+export default Header;
---
+++ src/components/Reminder.js
@@ -0,0 +1,18 @@
+import React, {} from "react";
+
+function Reminder(props) {
+    return (
+           <span> 
+                {/*  i. Display a reminder
+                           ii. It should be displayed as a <span> element if the showCompletedReminders is false
+                               OR                        a <s> (strike) element if the showCompletedReminders is true
+                
+                */} 
+
+                {props.showCompletedReminders ? (<s>{props.reminder.title}</s>) : (<span>{props.reminder.title}</span>)}
+
+            </span>
+    );
+}
+
+export default Reminder;
